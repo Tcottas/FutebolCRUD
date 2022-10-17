034380c710243273a3b2243e5fec06eb868ac1a6
Tcottas <tcottas@gmail.com>
2022-10-17 18:26:29 -0300

    Adicionar arquivos de projeto.

---
+++ WebMvcMysql/Migrations/20221014023349_Criacao-Inicial.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebMvcMysql.Migrations
+{
+    public partial class CriacaoInicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "Usuario",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    Nome = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Idade = table.Column<int>(type: "int", nullable: false),
+                    Posicao = table.Column<string>(type: "longtext", nullable: false)
+                        .Annotation("MySql:CharSet", "utf8mb4"),
+                    Salario = table.Column<double>(type: "double", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Usuario", x => x.Id);
+                })
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Usuario");
+        }
+    }
+}
---
+++ WebMvcMysql/Models/Usuario.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebMvcMysql.Models
+{
+    [Table("Usuario")]
+    public class Usuario
+    {
+
+        [Display(Name ="Código")]
+        [Column("Id")]
+        public int Id { get; set; }
+
+        [Display(Name = "Nome")]
+        [Column("Nome")]
+        [Required(ErrorMessage = "Nome é obrigatório.")]
+        public string Nome { get; set; }
+
+        [Display(Name = "Idade")]
+        [Column("Idade")]
+        [Required(ErrorMessage = "Idade é obrigatória.")]
+        public int Idade { get; set; }
+
+        [Display(Name = "Posição")]
+        [Column("Posicao")]
+        [Required(ErrorMessage = "Posição é obrigatória.")]
+        public string Posicao { get; set; }
+
+
+        [Display(Name = "Salário")]
+        [Column("Salario")]
+        [Required(ErrorMessage = "Valor é obrigatório.")]
+        [Range(0.01, 999999999, ErrorMessage = "Valor deve ser maior que 0. ")]
+        public double Salario { get; set; }
+
+    }
+}
